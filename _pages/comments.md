af7656e22b5b077a5a0162b7fb612ccb5004768f
kimjunseok <jnunseok@gmail.com>
2020-08-07 15:58:16 +0900

    feat: 최신 댓글 불러오기

--- _pages/comments.md
+++ _pages/comments.md
@@ -5,12 +5,13 @@ permalink: /comments/
 comment: false
 ---
 
-<div class="tag-header">
+<div class="comments-header">
     <h1>Comments</h1>
     <div class="user-comments"></div>
 </div>
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.19.0/axios.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/marked/1.1.1/marked.min.js" integrity="sha512-KCyhJjC9VsBYne93226gCA0Lb+VlrngllQqeCmX+HxBBHTC4HX2FYgEc6jT0oXYrLgvfglK49ktTTc0KVC1+gQ==" crossorigin="anonymous"></script>
 <script type="module">
     import { APIKEY } from './../assets/js/javable-api-key/authorization.js'
     import { commentsTemplate } from './../assets/js/utils/templates.js'
@@ -22,8 +23,8 @@ comment: false
     }).then(res => {
         const reverseData = res.data.reverse()
         reverseData.forEach(data => {
-            console.log(data)
             document.querySelector(".user-comments").insertAdjacentHTML('beforeend', commentsTemplate(data))
+            document.getElementById(`comment-body-${data.id}`).innerHTML = marked(`${data.body}`)
         })
     })
 </script>
--- assets/js/utils/templates.js
+++ assets/js/utils/templates.js
@@ -1,18 +1,15 @@
 export const commentsTemplate = data =>
+
     `
-        <div class="wrapper">
-            <div class="third card">
-                <div class="card-content">
-                    <div class="comment-card">
-                        <img src="${data.user.avatar_url}" class="round-icon"/>
-                        <div class="comment-wrapper">
-                            <div class="username">
-                                <a href="${data.user.html_url}">${data.user.login}</a> 
-                            </div>
-                            <p class="comment">${data.body}</p>
-                        </div>
-                    </div>
-                </div>
+        <div class="container">
+            <img src="${data.user.avatar_url}" class="container__pics left"/>
+            <p class="container__content">
+                <span class="container__content__username">${data.user.login}</span>
+                <span class="container__content__time">• il y a 2 heures</span>
+            </p>
+            <div class="container__content__text">
+                <p id="comment-body-${data.id}"></p>
             </div>
+            
         </div>
-    `;
+`
