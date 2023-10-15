bbf4de32571a8b92dd9f6c10efe155f31313c340
cruelladevil <dev.timetravel@gmail.com>
2023-10-16 04:08:41 +0900

    fix: 404 페이지에서 홈으로 가는 링크 수정

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -41,7 +41,7 @@ const NotFoundPage: React.FC<NotFoundTemplateProps> = props => {
             <section style={{ textAlign: 'center' }}>
               <ErrorCode>404</ErrorCode>
               <ErrorDescription>Page not found</ErrorDescription>
-              <Link css={ErrorLink} to="">
+              <Link css={ErrorLink} to="/">
                 Go to the front page →
               </Link>
             </section>
