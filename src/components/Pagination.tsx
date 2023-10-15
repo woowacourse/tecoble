98ae9fc865ee5ea48d2131af025049521400a840
cruelladevil <dev.timetravel@gmail.com>
2023-10-16 04:12:25 +0900

    fix: Pagination 이슈 해결
    - issue: https://github.com/scttcper/gatsby-casper/issues/111
    - PR: https://github.com/scttcper/gatsby-casper/pull/112

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -13,8 +13,8 @@ export interface PaginationProps {
 const Pagination: React.FunctionComponent<PaginationProps> = ({ currentPage, numPages }) => {
   const isFirst = currentPage === 1;
   const isLast = currentPage === numPages;
-  const prevPage = currentPage - 1 === 1 ? '/' : (currentPage - 1).toString();
-  const nextPage = (currentPage + 1).toString();
+  const prevPage = currentPage - 1 === 1 ? '/' : `../${(currentPage - 1).toString()}`;
+  const nextPage = `../${(currentPage + 1).toString()}`;
 
   return (
     <nav css={navCss}>
